447511c59dc148eb2ea07fb92845eafd1a3242c2
Markswell <osoromarkswell80@gmail.com>
2024-04-23 13:20:13 +0300

    Style Home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Jokes from './components/jokes/Jokes';
 import Category from './components/categories/Category';
 import CategoryList from './components/categoryList/CategoryList';
-
+import './App.css'; 
 
 const App = () => {
+  // State variable to manage theme
+  const [theme, setTheme] = useState('green');
+
+  // Function to toggle theme between green and yellow
+  const toggleTheme = () => {
+    setTheme(theme === 'green' ? 'yellow' : 'green');
+  };
 
   return (
-    <div>
-      <br></br>
-      <Jokes/>
-      <br></br>
+    <div className={`app-container ${theme}`}>
+      {/* Title with blinking effect and emojis */}
+      <h1 className="title">
+        <span role="img" aria-label="laughing face">😆</span>
+        CHUCK NORRIS JOKES
+        <span role="img" aria-label="winking face">😉</span>
+      </h1>
+      {/* Button to toggle theme */}
+      <button className="theme-button" onClick={toggleTheme}>Change Theme</button>
+      
+      {/* Components */}
+      <Jokes />
+      <br />
       <Category />
-      <br></br>
+      <br />
       <CategoryList />
-      <br></br>
+      <br />
     </div>
   );
 };
